71172978df34e2f86b47291a323228307fdb91be
alpersad <alpersad@uwaterloo.ca>
2020-06-16 01:13:35 -0400

    added portal-vue and uuid

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import SuiVue from 'semantic-ui-vue';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import SuiVue from "semantic-ui-vue";
+import PortalVue from "portal-vue";
+import UUID from "vue-uuid";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(SuiVue);
+Vue.use(PortalVue);
+Vue.use(UUID);
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
