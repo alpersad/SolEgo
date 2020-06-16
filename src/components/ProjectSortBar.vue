364a45550df9781a1665cfcfb9a1eff65cd2a90c
alpersad <alpersad@uwaterloo.ca>
2020-06-16 01:16:37 -0400

    minor changes

--- src/components/ProjectSortBar.vue
+++ src/components/ProjectSortBar.vue
@@ -1,26 +1,41 @@
 <template>
-    <div class="button-layout">
-        <sui-button-group size="large">
-            <sui-button v-on:click="$emit('sort-projects','all')" v-bind:key='all' :content="All" />
-            <sui-button v-on:click="$emit('sort-projects',tech)" v-bind:key="tech" v-for="tech in techlist" :content="tech" />
-        </sui-button-group>
-    </div>
+  <div class="button-layout">
+    <sui-divider horizontal>Filter</sui-divider>
+    <sui-button-group size="large">
+      <sui-button
+        class="button-text"
+        v-on:click="$emit('sort-projects', all)"
+        v-bind:key="all"
+        :content="all"
+      />
+      <sui-button
+        class="button-text"
+        v-on:click="$emit('sort-projects', tech)"
+        v-bind:key="tech"
+        v-for="tech in techlist"
+        :content="tech"
+      />
+    </sui-button-group>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'ProjectSortBar',
-    props: ["techlist"],
-    data() {
-        return {
-            All: "All"
-        }
-    }
-}
+  name: "ProjectSortBar",
+  props: ["techlist"],
+  data() {
+    return {
+      all: "all"
+    };
+  }
+};
 </script>
 
 <style scoped>
-    .button-layout{
-        padding-bottom: 2%;
-    }
+.button-layout {
+  padding-bottom: 2%;
+}
+.button-text {
+  text-transform: capitalize;
+}
 </style>
--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,54 +1,55 @@
 <template>
-    <div id="nav">
-        <div>
-            <router-link to="/"><img src="../../assets/kaguya.png" alt="Profile Picture" class="propic" ></router-link>
-        </div>
-        <div class="nav-align">
-            <router-link class="nav-text" to="/">Home</router-link> 
-            <router-link class="nav-text" to="/about">About</router-link> 
-            <router-link class="nav-text" to="/projects">Projects</router-link>
-        </div>
-        <router-view/>
+  <div id="nav">
+    <div>
+      <router-link to="/"
+        ><img src="../../assets/kaguya.png" alt="Profile Picture" class="propic"
+      /></router-link>
     </div>
+    <div class="nav-align">
+      <router-link class="nav-text" to="/">Home</router-link>
+      <router-link class="nav-text" to="/about">About</router-link>
+      <router-link class="nav-text" to="/projects">Projects</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'NavBar'
-}
+  name: "NavBar",
+};
 </script>
 
 <style scoped>
-    #nav {
-        height: 100px;
-        width: auto;
-        background-color: #261421;
-        border-bottom-style: solid;
-        border-color: #D93662;
-        text-align: left;
-    }
-    .nav-text {
-        font-size: 24px;
-        color: white;
-        margin: 20px;
-    }
-    .nav-align {
-        padding-top: 50px;
-        margin-left: 250px;
-    }
-    a:hover {
-        color: gray;
-        text-decoration: underline;
-    }
-    .propic {
-        height:150px;
-        width: 150px;
-        float: left;
-        margin-top: 30px;
-        margin-left: 50px;
-    }
-    .router-link-exact-active {
-        color: gray;
-        text-decoration: underline;
-    }
+#nav {
+  height: 100px;
+  width: auto;
+  background-color: #261421;
+  border-bottom-style: solid;
+  border-color: #d93662;
+  text-align: left;
+}
+.nav-text {
+  font-size: 24px;
+  color: white;
+  margin: 20px;
+}
+.nav-align {
+  padding-top: 50px;
+  margin-left: 250px;
+}
+a:hover {
+  color: gray;
+  text-decoration: underline;
+}
+.propic {
+  height: 150px;
+  width: 150px;
+  float: left;
+  margin-top: 30px;
+  margin-left: 50px;
+}
+.router-link-exact-active {
+  color: gray;
+  text-decoration: underline;
+}
 </style>
