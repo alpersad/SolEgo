5903ff209fcf97624eab214b213b9495bde05f74
alpersad <alpersad@uwaterloo.ca>
2020-06-16 01:15:30 -0400

    updated card layout

--- src/components/ProjectGroup.vue
+++ src/components/ProjectGroup.vue
@@ -1,36 +1,67 @@
 <template>
-    <!-- TODO -->
-    <!-- DESIGN CARD LAYOUT -->
-    <div>
-        <sui-card-group :items-per-row="3" >
-                <sui-card v-bind:key="projectitem.id" v-for="projectitem in projects" v-bind:projectitem="projectitem" class=" ">
-                    <sui-card-content>
-                        <sui-card-header>{{projectitem.title}}</sui-card-header>
+  <div>
+    <sui-divider horizontal>Projects</sui-divider>
+    <sui-card-group :items-per-row="3">
+      <sui-card
+        v-bind:key="projectitem.id"
+        v-for="projectitem in projects"
+        v-bind:projectitem="projectitem"
+        class="raised"
+      >
+        <sui-card-content>
+          <sui-card-header>{{ projectitem.title }}</sui-card-header>
+          <sui-card-meta class="techstack">{{
+            projectitem.tech.map((x) => " " + x).toString()
+          }}</sui-card-meta>
+        </sui-card-content>
 
-                        <!-- Generates a string where each technology used has its first letter capitalized and a space is added between multiple technologies -->
-                        <sui-card-meta>{{projectitem.tech.map( x => " " + x.charAt(0).toUpperCase() + x.slice(1)).toString()}}</sui-card-meta>
+        <sui-card-content>
+          <sui-card-description class="card-desc">{{
+            projectitem.description
+          }}</sui-card-description>
+        </sui-card-content>
 
-                        <sui-card-description>
-                            {{projectitem.description}}
-                        </sui-card-description>
-                    </sui-card-content>
-                    <sui-button attached="bottom" size="small">
-                        <sui-icon  name="github" size="big" link/>
-                    </sui-button>
-                </sui-card>
-                
-        </sui-card-group>
-        
-    </div>
+        <sui-button-group>
+          <sui-button
+            v-on:click="link(projectitem.github)"
+            attached="bottom"
+            size="small"
+          >
+            <sui-icon name="github" size="big" link />
+          </sui-button>
+          <sui-popup
+            wide
+            basic
+            position="top center"
+            content="Email arunlevipersad@gmail.com for download"
+          >
+            <sui-button attached="bottom" size="small" slot="trigger">
+              <sui-icon name="download" size="big" link />
+            </sui-button>
+          </sui-popup>
+        </sui-button-group>
+      </sui-card>
+    </sui-card-group>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: 'ProjectGroup',
-    props: ["projects"]
-}
+  name: "ProjectGroup",
+  props: ["projects"],
+  methods: {
+    link: function(link) {
+      window.open(link);
+    },
+  },
+};
 </script>
 
 <style scoped>
+.techstack {
+  text-transform: capitalize;
+}
+.card-desc {
+  text-align: left;
+}
 </style>
