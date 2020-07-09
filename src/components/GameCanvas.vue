d58948700163fc40465067a44810043916e3fefa
alpersad <alpersad@uwaterloo.ca>
2020-07-09 00:05:15 -0400

    completed runaway love

--- src/components/GameCanvas.vue
+++ src/components/GameCanvas.vue
@@ -17,8 +17,19 @@ export default {
     path: null,
     scope: null,
     player: null,
-    love: null,
-    canvasId: "game"
+    currentItem: null,
+    canvasId: "game",
+    levelParameters: {
+      start: {},
+      smart: {},
+      emotions: {},
+      love: {
+        interest: null,
+        seen: false
+      },
+      sadness: {},
+      end: {}
+    }
   }),
   methods: {
     reset() {
@@ -30,73 +41,182 @@ export default {
     },
     mouseDown() {
       this.tool = this.createTool(this.scope);
-      var currentItem = null;
       this.tool.onMouseDown = event => {
-        var hitResult = this.scope.project.hitTest(event.point);
+        var hitResult = this.scope.project.hitTest(event.point); // Test for item hit
+        if (this.currentItem) {
+          this.currentItem.selected = false; // De-select current selected item if user clicks empty space
+        }
         if (!hitResult) {
           return;
         }
-        currentItem = hitResult.item;
-        currentItem.selected = true;
-        if (currentItem.name === "player") {
+        this.currentItem = hitResult.item;
+        this.currentItem.selected = true; // Selects item hit
+        if (this.currentItem.name === "player") {
           this.$emit("selected", "PLAYER");
+        }
+        switch (this.currentItem.name) {
+          case "player":
+            this.$emit("selected", "PLAYER");
+            break;
+          default:
+            console.log("Error on select emit");
         }
       };
       this.tool.onMouseDrag = event => {
-        if (currentItem) {
-          currentItem.position = event.point;
+        if (this.currentItem.name === "player") {
+          // Allows only Player item to be dragged
+
+          this.currentItem.position = event.point;
+
+          // var dragDirection = event.point.subtract(this.currentItem.position);
+          // this.currentItem.position = this.currentItem.position.add(
+          //   dragDirection.normalize()
+          // );
+          // console.log(dragDirection, this.currentItem.position);
         }
       };
       this.tool.onMouseUp = () => {
-        if (currentItem) {
-          currentItem.selected = false;
-          currentItem = null;
-        }
+        return;
       };
     },
-    // mouseDown() {
-    //   console.log("mouseover");
-    //   // in order to access functions in nested tool
-    //   let self = this;
-    //   // create drawing tool
-    //   this.tool = this.createTool(this.scope);
-    //   this.tool.onMouseDown = event => {
-    //     // init path
-    //     self.path = self.pathCreate(self.scope);
-    //     // add point to path
-    //     self.path.add(event.point);
-    //   };
-    //   this.tool.onMouseDrag = event => {
-    //     self.path.add(event);
-    //   };
-    //   this.tool.onMouseUp = event => {
-    //     // line completed
-    //     self.path.add(event.point);
-    //   };
-    // },
     drawLove(scope) {
+      // Create LoveInterest item
       scope.activate();
       // Build path.
       var top = [20, 80];
       var bottom = [100, 80];
       var left = [60, 40];
       var right = [60, 120];
-      this.love = new paper.Path({
+      this.levelParameters.love.interest = new paper.Path({
         segments: [top, right, bottom, left],
         fillColor: paper.Color.random(),
-        closed: true
+        closed: true,
+        name: "loveInterest"
       });
+
+      return this.levelParameters.love.interest;
+    },
+    createLevelStart(scope) {
+      return scope;
+    },
+    createLevelSmart(scope) {
+      return scope;
+    },
+    createLevelEmotions(scope) {
+      return scope;
+    },
+    createLevelLove(scope) {
+      var loveInterest = this.drawLove(scope); // Draw item on canvas
+      scope.view.onFrame = () => {
+        // Check if loveInterest item is "seen"
+        if (loveInterest.position.isClose(this.player.position, 100)) {
+          this.levelParameters.love.seen = true;
+        }
+        if (this.levelParameters.love.seen) {
+          // Calculate direction from LoveInterest item to Player item
+          var runAwayDirection = this.player.position.subtract(
+            loveInterest.position
+          );
+
+          // Get normal vector of direction from LoveInterest item to Player item
+          // Rotate vector 180 degrees
+          runAwayDirection = runAwayDirection.normalize().rotate(180);
+
+          // Move LoveInterest in the opposite direction of Player
+          loveInterest.position = loveInterest.position.add(
+            runAwayDirection.multiply(loveInterest.bounds.width / 50)
+          );
+        } else {
+          // Move item across canvas
+          loveInterest.position.x += loveInterest.bounds.width / 50;
+        }
+        // Handle item leaving the canvas
+        if (
+          loveInterest.bounds.left > scope.view.size.width ||
+          loveInterest.bounds.right < 0 ||
+          loveInterest.bounds.top > scope.view.size.height ||
+          loveInterest.bounds.bottom < 0
+        ) {
+          loveInterest.fillColor = paper.Color.random();
+          this.levelParameters.love.seen = false; // resets if LoveInterest item has "seen" Player item
+          loveInterest.position.x = -loveInterest.bounds.width / 2 + 1; // Reset item to left side of screen
+          loveInterest.position.y =
+            scope.Point.random().y * scope.view.size.height; // Randomly choose item's animation height on canvas
+
+          // Handle cases where item draws off screen
+          if (
+            loveInterest.position.y >
+            scope.view.size.height - loveInterest.bounds.height
+          ) {
+            loveInterest.position.y =
+              scope.view.size.height - loveInterest.bounds.height;
+          }
+          if (loveInterest.position.y < loveInterest.bounds.height) {
+            loveInterest.position.y = loveInterest.bounds.height;
+          }
+        }
+      };
+    },
+    createLevelSadness(scope) {
+      return scope;
+    },
+    createLevelEnd(scope) {
+      return scope;
+    },
+    createPlayer(scope) {
+      scope.activate();
+      //define player model
+      this.player = new paper.Path.RegularPolygon(
+        new paper.Point(80, 70),
+        3,
+        25
+      );
+      this.player.fillColor = "#000000";
+      this.player.name = "player";
+      this.player.position = this.scope.view.center;
     }
   },
   mounted() {
+    // Initialize and mount canvas
     this.scope = new paper.PaperScope();
     this.scope.setup(this.canvasId);
 
-    this.player = new paper.Path.RegularPolygon(new paper.Point(80, 70), 3, 25);
-    this.player.fillColor = "#e9e9ff";
-    this.player.name = "player";
-    console.log(LEVEL.SMART);
-    this.drawLove(this.scope);
+    // Create and draw player on canvas
+    this.createPlayer(this.scope);
+
+    // Generate level
+    switch (this.level) {
+      case LEVEL.START:
+        this.createLevelStart(this.scope);
+        console.log("start");
+        break;
+      case LEVEL.SMART:
+        this.createLevelSmart(this.scope);
+        console.log("smart");
+        break;
+      case LEVEL.EMOTIONS:
+        this.createLevelEmotions(this.scope);
+        console.log("emotions");
+        break;
+      case LEVEL.LOVE:
+        this.createLevelLove(this.scope);
+        console.log("love");
+        break;
+      case LEVEL.SADNESS:
+        this.createLevelSadness(this.scope);
+        console.log("sadness");
+        break;
+      case LEVEL.END:
+        this.createLevelEnd(this.scope);
+        console.log("end");
+        break;
+      default:
+        console.log("ERROR");
+    }
+
+    // this.scope.view.onFrame = () => {
+    //   this.player.rotate(2);
+    // };
     // this.scope.view.viewSize.set(400, 600);
     // this.scope.view.onFrame = event => {};
   }
